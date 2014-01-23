c4db2d89688cf905939ba424477dd02ce18c0da4
Robin Franken <franken@lequest.nl>
2014-01-23 14:20:36 +0100

    App is now using data-persistency

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,13 +1,12 @@
 ContactManager.module('ContactsApp.Show', function(Show, ContactManager, Backbone, Marionette, $, _){
     Show.Controller = {
         showContact: function(id){
-            var contacts = ContactManager.request('contact:entities');
-            var model = contacts.get(id);
+            var contacts = ContactManager.request('contact:entities', id);
 
             var contactView;
-            if(typeof model !== 'undefined'){
+            if(typeof contact !== 'undefined'){
                 contactView = new Show.Contact({
-                    model: model
+                    model: contact
                 });
             }else{
                 contactView = new Show.MissingContact();
--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,10 +1,17 @@
 ContactManager.module('Entities', function (Entities, ContactManager, Backbone, Marionette, $, _) {
-    Entities.Contact = Backbone.Model.extend({});
+    Entities.Contact = Backbone.Model.extend({
+        urlRoot: 'contacts'
+    });
+
+    Entities.configureStorage(Entities.Contact);
 
     Entities.ContactCollection = Backbone.Collection.extend({
+        url: 'contacts',
         model: Entities.Contact,
         comparator: 'firstName'
     });
+
+    Entities.configureStorage(Entities.ContactCollection);
 
     var contacts;
 
@@ -14,18 +21,34 @@ ContactManager.module('Entities', function (Entities, ContactManager, Backbone, 
             {id: 2, firstName: 'Bob', lastName: 'Brigham', phoneNumber: '555-0163'},
             {id: 3, firstName: 'Charlie', lastName: 'Campbell', phoneNumber: '555-0129'}
         ]);
+        contacts.forEach(function(contact){
+            contact.save();
+        });
+        return contacts;
     }
 
     var API = {
         getContactEntities: function () {
-            if (typeof contacts === 'undefined') {
-                initializeContacts();
+            var contacts = new Entities.ContactCollection();
+            contacts.fetch();
+            if(contacts.length === 0){
+                return initializeContacts();
             }
             return contacts;
+        },
+
+        getContactEntity: function(contactId){
+            var contact = new Entites.Contact({id: contactId});
+            contact.fetch();
+            return contact;
         }
     }
 
     ContactManager.reqres.setHandler('contact:entities', function(){
         return API.getContactEntities();
+    });
+
+    ContactManager.reqres.setHandler('contact:entity', function(id){
+        return API.getContactEntity(id);
     });
 });
