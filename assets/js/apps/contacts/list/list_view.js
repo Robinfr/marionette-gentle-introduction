a34f07e99ac1ac4eda0c1765a27b428cae2175a2
Robin Franken <franken@lequest.nl>
2014-01-23 11:00:24 +0100

    Now using tables and composite view

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,11 +1,14 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
     List.Contact = Marionette.ItemView.extend({
         template: '#contact-list-item',
-        tagName: 'li'
+        tagName: 'tr'
     });
 
-    List.Contacts = Marionette.CollectionView.extend({
-        tagName: 'ul',
-        itemView: List.Contact
+    List.Contacts = Marionette.CompositeView.extend({
+        tagName: 'table',
+        className:'table table-hover',
+        template: '#contact-list',
+        itemView: List.Contact,
+        itemViewContainer: 'tbody'
     });
 });
