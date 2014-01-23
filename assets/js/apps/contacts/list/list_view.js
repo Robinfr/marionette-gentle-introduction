09cff81269fb4d016a765e139e2a7c42e62c1b2f
Robin Franken <franken@lequest.nl>
2014-01-23 11:34:05 +0100

    Added delete

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,28 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
     List.Contact = Marionette.ItemView.extend({
         template: '#contact-list-item',
-        tagName: 'tr'
+        tagName: 'tr',
+
+        events: {
+            'click': 'highlightName',
+            'click button.js-delete': 'deleteClicked'
+        },
+
+        highlightName: function(){
+            this.$el.toggleClass('warning');
+        },
+
+        deleteClicked: function(e){
+            e.stopPropagation();
+            this.trigger('contact:delete', this.model);
+        },
+
+        remove: function(){
+            var self = this;
+            this.$el.fadeOut(function(){
+                Marionette.ItemView.prototype.remove.call(self);
+            });
+        }
     });
 
     List.Contacts = Marionette.CompositeView.extend({
