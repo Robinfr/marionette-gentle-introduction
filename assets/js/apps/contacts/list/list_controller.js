78cbf6df06fe01a51c6f6f79dff337ae4734b0d9
Robin Franken <franken@lequest.nl>
2014-01-24 11:13:56 +0100

    We can now add new contacts! Also the Form is a common view

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -15,9 +15,36 @@ ContactManager.module('ContactsApp.List', function (List, ContactManager, Backbo
                     collection: contacts
                 });
 
-                contactsListLayout.on('show', function(){
+                contactsListLayout.on('show', function () {
                     contactsListLayout.panelRegion.show(contactsListPanel);
                     contactsListLayout.contactsRegion.show(contactsListView);
+
+                    contactsListPanel.on('contact:new', function () {
+                        var newContact = new ContactManager.Entities.Contact();
+
+                        var view = new ContactManager.ContactsApp.New.Contact({
+                            model: newContact,
+                            asModal: true
+                        });
+
+                        view.on('form:submit', function (data) {
+                            var highestId = contacts.max(function (c) {
+                                return c.id;
+                            });
+                            highestId = highestId.get('id');
+                            data.id = highestId + 1;
+                            if (newContact.save(data)) {
+                                contacts.add(newContact);
+                                ContactManager.dialogRegion.close();
+                                var $view = contactsListView.children.findByModel(newContact);
+                                $view.flash('success');
+                            } else {
+                                view.triggerMethod('form:data:invalid', newContact.validationError);
+                            }
+                        });
+
+                        ContactManager.dialogRegion.show(view);
+                    });
                 });
 
                 contactsListView.on('itemview:contact:delete', function (childView, model) {
@@ -28,18 +55,18 @@ ContactManager.module('ContactsApp.List', function (List, ContactManager, Backbo
                     ContactManager.trigger('contact:show', model.get('id'));
                 });
 
-                contactsListView.on('itemview:contact:edit', function(childView, model){
+                contactsListView.on('itemview:contact:edit', function (childView, model) {
                     var view = new ContactManager.ContactsApp.Edit.Contact({
                         model: model,
                         asModal: true
                     });
 
-                    view.on('form:submit', function(data){
-                        if(model.save(data)){
+                    view.on('form:submit', function (data) {
+                        if (model.save(data)) {
                             childView.render();
                             ContactManager.dialogRegion.close();
                             childView.flash('success');
-                        }else{
+                        } else {
                             view.triggerMethod('form:data:invalid', model.validationError);
                         }
                     });
